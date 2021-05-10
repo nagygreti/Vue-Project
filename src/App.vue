f3edfdde666e47595cb3b1a4b8f828fc94ab4fce
Nagy <margareta.nagy@vodafone.com>
2021-05-10 11:49:20 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-btn text>
+        <h1 class="pointer headline" @click="$router.push('/')">
+          Vue Exercise
+        </h1>
+      </v-btn>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
